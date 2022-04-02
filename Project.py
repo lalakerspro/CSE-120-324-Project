1e1b6a90574d96954e09a77fec8dce3570bed428
lalakerspro <vvyas@ucmerced.edu>
2022-04-01 22:12:23 -0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,23 +1,21 @@
 
 class Camera:
   def configure_camera()
-  
-  
+
+
 
 class RetrieveImages:
-   def retrieve_font()
+   def retrieve_SN()
    def retrieve_colors()
    def retrieve_image()
-  
-  
+
+
 
 class InterpretImages:
    def compare_font()
    def compare_colors()
 
-  
+
 
 class OutputResults:
    def determine_legibility()
-
-
