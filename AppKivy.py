5696bdedc5c7bec7c04015d66de46f9d413762ab
Angel Monroy Jr <angelmonroyjr@gmail.com>
2022-04-09 19:30:37 -0700

    Fixed a typo

--- AppKivy.py
+++ AppKivy.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.camera import Camera
-from kivy.uix.gridlayout import GridyLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
 
