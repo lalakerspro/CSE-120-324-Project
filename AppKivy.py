265c175684a7ceeade7d38c9b4b25ac27a3fe443
Angel Monroy Jr <angelmonroyjr@gmail.com>
2022-04-09 19:03:57 -0700

    Create button and its layout for the camera app

--- AppKivy.py
+++ AppKivy.py
@@ -7,3 +7,14 @@ from kivy.core.window import Window
 Window.size = (500,550)
 
 class cameraApp(App) :
+
+    def build (self):
+        global cameraApp
+        cam = Camera()
+
+        btn = Button (text="Capture Image")
+        btn.size_hint = (.1,.1)
+        btn.font_size = 35
+        btn.background_color = 'blue'
+        btn.bind (on_press = self.capture_image)
+        
