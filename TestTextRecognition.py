676d3f1f1df04ed6a8ce3d84d5dc3172c9fa85c0
Angel Monroy Jr <angelmonroyjr@gmail.com>
2022-04-10 17:48:08 -0700

    Fixed spacing

--- TestTextRecognition.py
+++ TestTextRecognition.py
@@ -4,7 +4,6 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-
 image = Image.open('F:/imagess.jpg')
 
 image_to_text = pytesseract.image_to_string(image, lang='eng')
