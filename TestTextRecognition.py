8b690935c51f34e2c32640d1bed55fc9d793c7af
Angel Monroy Jr <angelmonroyjr@gmail.com>
2022-04-10 17:54:57 -0700

    Fix unicodeescape error

--- TestTextRecognition.py
+++ TestTextRecognition.py
@@ -4,7 +4,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image = Image.open('C:\Users\angel\Desktop\imagess.jpg')
+image = Image.open('C:/Users/angel/Desktop/imagess.jpg')
 
 image_to_text = pytesseract.image_to_string(image, lang='eng')
 
