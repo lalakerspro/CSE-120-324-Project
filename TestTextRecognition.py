772d7863e9dc248232a8b49c5b7adb920875f8ad
Angel Monroy Jr <angelmonroyjr@gmail.com>
2022-04-10 17:50:57 -0700

    Fix location of open images

--- TestTextRecognition.py
+++ TestTextRecognition.py
@@ -4,9 +4,10 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image = Image.open('F:/imagess.jpg')
+image = Image.open('C:\Users\angel\Desktop\imagess.jpg')
 
 image_to_text = pytesseract.image_to_string(image, lang='eng')
 
 print (image_to_text)
+
 
