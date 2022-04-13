332ee1373624566e09572490aabd64648647fbb4
lalakerspro <vvyas@ucmerced.edu>
2022-04-13 02:43:47 -0700

    testing OCR

--- PytesseractTest.py
+++ PytesseractTest.py
@@ -4,10 +4,8 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image = Image.open('C:/Users/angel/Desktop/imagess.jpg')
+image = Image.open('test.png')
 
 image_to_text = pytesseract.image_to_string(image, lang='eng')
 
-print (image_to_text)
-
-
+print (image_to_text+"hi")
