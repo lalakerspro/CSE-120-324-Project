8653db3a68d51542a59b4f315c9a6c709d3eeeb0
lalakerspro <vvyas@ucmerced.edu>
2022-04-13 12:21:24 -0700

    Integrate With Webcam

--- PytesseractTest.py
+++ PytesseractTest.py
@@ -4,7 +4,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-image = Image.open('test.png')
+image = Image.open('test2.png')
 
 image_to_text = pytesseract.image_to_string(image, lang='eng')
 
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+'''
+Camera Example
+==============
+
+This example demonstrates a simple use of the camera. It shows a window with
+a buttoned labelled 'play' to turn the camera on and off. Note that
+not finding a camera, perhaps because gstreamer is not installed, will
+throw an exception during the kv language processing.
+
+'''
+
+# Uncomment these lines to see all the messages
+# from kivy.logger import Logger
+# import logging
+# Logger.setLevel(logging.TRACE)
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 import time
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+from PIL import Image
+
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 Builder.load_string('''
 <CameraClick>:
     orientation: 'vertical'
@@ -19,17 +44,25 @@ Builder.load_string('''
         text: 'Capture'
         size_hint_y: None
         height: '48dp'
-        on_press: root.capture()
+        on_press:
+            root.capture()
+            root.test()
 ''')
 
 
 class CameraClick(BoxLayout):
     def capture(self):
-
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
-        #camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
+        c=camera.export_to_png("IMG.png")
+        return c;
+    def test(self):
+        image = Image.open('IMG.PNG')
+        print(pytesseract.image_to_string(image, lang='eng'))
 
 
 class TestCamera(App):
