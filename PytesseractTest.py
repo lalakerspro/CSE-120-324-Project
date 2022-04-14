25a4ea1ea5a6986360881e91cb9d147de2cfe817
lalakerspro <vvyas@ucmerced.edu>
2022-04-13 21:38:35 -0700

    Replace Image

--- PytesseractTest.py
+++ PytesseractTest.py
@@ -9,3 +9,8 @@ image = Image.open('test2.png')
 image_to_text = pytesseract.image_to_string(image, lang='eng')
 
 print (image_to_text+"hi")
+
+#def test(self):
+        #image = Image.open('IMG.PNG')
+        #print(pytesseract.image_to_string(image, lang='eng'))
+
