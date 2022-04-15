258a5bf90daae93db36099af014a99535c66a00a
lalakerspro <vvyas@ucmerced.edu>
2022-04-15 06:49:05 -0700

    basic app

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
+import numpy as np
 import cv2
-from PIL import Image
+from PIL import ImageFont, ImageDraw, Image 
 
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-  
-  
-# define a video capture object
+
+
 cam = cv2.VideoCapture(0)
-  
-while(True):
-      
-    # Capture the video frame
-    # by frame
-    ret, frame = vid.read()
-  
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-      
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
         break
-  
-# After the loop release the cap object
-vid.release()
-# Destroy all the windows
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        image=frame;
+        image_to_text = pytesseract.image_to_string(image, lang='eng')
+        print (image_to_text+"\n____________________________")
+        ascii_values = []
+        special_characters = "!@#$%^&*()-+?_=,<>/"
+        leg="legible"
+        nonleg="nonlegible"
+        if any(c in special_characters for c in image_to_text):
+            bool=leg
+        else:
+            bool=nonleg
+        coordinates = (100,100)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 1
+        color = (255,0,0)
+        thickness = 2
+        image = cv2.putText(image, bool, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
+        cv2.imshow("Text", image)
+   
+# Displaying the image
+cv2.imshow(window_name, image) 
+
+cam.release()
+
 cv2.destroyAllWindows()
