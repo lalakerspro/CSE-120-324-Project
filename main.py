695591c0922a771c3019bb7019499308e04f7199
lalakerspro <vvyas@ucmerced.edu>
2022-04-27 14:31:27 -0700

    Working App!

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+from imutils import paths
+import argparse
+import sys
 import numpy as np
 import cv2
 from PIL import ImageFont, ImageDraw, Image 
 
-import pytesseract
+
 
 #Database import as well as other necessary imports
 import Database
 import datetime
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+
 
 img_counter = 0
 
@@ -22,7 +24,11 @@ while True:
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
+    cv2.namedWindow("SpringScan", cv2.WINDOW_NORMAL)
+    cv2.moveWindow("SpringScan", 100,100);
+ 
+    cv2.imshow("SpringScan", frame)
+    
 
     k = cv2.waitKey(1)
     if k%256 == 27:
@@ -32,37 +38,41 @@ while True:
     elif k%256 == 32:
         # SPACE pressed
         image=frame;
-        image_to_text = pytesseract.image_to_string(image, lang='eng')
-        print (image_to_text+"\n____________________________")
-        ascii_values = []
-        special_characters = "!@#$%^&*()-+?_=,<>/"
-        leg="legible"
-        nonleg="nonlegible"
-        if any(c in special_characters for c in image_to_text):
-            bool=leg
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fm = cv2.Laplacian(image, cv2.CV_64F).var()
+        print(fm)
+        leg = cv2.imread('leg.png')
+        nonleg = cv2.imread('nonleg.png')
+        if fm <90:
+            result=nonleg
         else:
-            bool=nonleg
-        coordinates = (100,100)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 1
-        color = (255,0,0)
-        thickness = 2
-        image = cv2.putText(image, bool, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
-        cv2.imshow("Text", image)
+            result=leg
+
+        position = (10,50)
+        cv2.putText(
+            image, #numpy array on which text is written
+            "CAPTURED SPRING:", #text
+            position, #position at which writing has to start
+            cv2.FONT_HERSHEY_SIMPLEX, #font family
+            1, #font size
+            (0,204,0), #font color
+            3) #font stroke
+
+        cv2.imshow('CAPTURED SPRING', image)
+        cv2.imshow('Result', result)
+
    
 # Displaying the image
-cv2.imshow(window_name, image)
 
 
 
 #I am assuming image_to_text is the serial
 #Inserting Data into database
 #Example: insertDATA("19:51/4/14/22", "S158392", "C:\Users\Blood\source\repos\CSE-120-324-Project\IMG.png")
-img_name = datetime.datetime
-img_name_jpg = img_name+".jpg"
-cv2.imwrite(img_name, image)
-insertDATA(datetime.datetime, image_to_text, img_name_jpg)
+#img_name = datetime.datetime
+#img_name_jpg = img_name+".jpg"
+#cv2.imwrite(img_name, image)
+#insertDATA(datetime.datetime, image_to_text, img_name_jpg)
 
 cam.release()
-
 cv2.destroyAllWindows()
