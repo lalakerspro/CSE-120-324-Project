575a509221f62101886df8bce90a8638b7b5e488
lalakerspro <vvyas@ucmerced.edu>
2022-04-29 16:47:55 -0700

    fixed image storing!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import numpy as np
 import cv2
+import os
 from PIL import ImageFont, ImageDraw, Image 
 
 
@@ -11,13 +12,14 @@ from PIL import ImageFont, ImageDraw, Image
 import Database
 import datetime
 
+i=0
 
-
+#starting camera
 cam = cv2.VideoCapture(0)
 
 
 
-img_counter = 0
+i = 0
 
 while True:
     ret, frame = cam.read()
@@ -32,46 +34,54 @@ while True:
 
     k = cv2.waitKey(1)
     if k%256 == 27:
-        # ESC pressed
         print("Escape hit, closing...")
         break
     elif k%256 == 32:
-        # SPACE pressed
         image=frame;
-        fm = cv2.Laplacian(image, cv2.CV_64F).var()
-        print(fm)
+        #calculating legibility threshold
+        thresh = cv2.Laplacian(image, cv2.CV_64F).var()
         leg = cv2.imread('leg.png')
         nonleg = cv2.imread('nonleg.png')
-        if fm <40:
+        path = 'images'
+        #writing the image to a file
+        img_name = "spring{}.png".format(i+1)
+        cv2.imwrite(os.path.join(path , img_name), frame)
+        cv2.waitKey(0)
+        i += 1
+        
+        #determining if image is legible based on threshold
+        if thresh <90:
+            fontcolor=(0,0,255)
             result=nonleg
         else:
+            fontcolor=(0,204,0)
             result=leg
            
+        #outputting captured spring   
         position = (10,50)
         cv2.putText(
-            image, #numpy array on which text is written
-            "CAPTURED SPRING:", #text
-            position, #position at which writing has to start
-            cv2.FONT_HERSHEY_SIMPLEX, #font family
-            1, #font size
-            (0,204,0), #font color
-            3) #font stroke
+            image, 
+            "CAPTURED SPRING:", 
+            position, 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            1, 
+            fontcolor, 
+            3) 
 
         cv2.imshow('CAPTURED SPRING', image)
+        #showing legibility
         cv2.imshow('Result', result)
 
    
 # Displaying the image
 
+img_name = datetime.datetime
+img_name_jpg = img_name+".jpg"
+cv2.imwrite(img_name, image)
+insertDATA(datetime.datetime, image_to_text, img_name_jpg)
 
 
-#I am assuming image_to_text is the serial
-#Inserting Data into database
-#Example: insertDATA("19:51/4/14/22", "S158392", "C:\Users\Blood\source\repos\CSE-120-324-Project\IMG.png")
-#img_name = datetime.datetime
-#img_name_jpg = img_name+".jpg"
-#cv2.imwrite(img_name, image)
-#insertDATA(datetime.datetime, image_to_text, img_name_jpg)
+
 
 cam.release()
 cv2.destroyAllWindows()
